06b6b9778871019fa2730954b77c6838aaa1cdce
Sumanth1728 <73275822+Sumanth1728@users.noreply.github.com>
2020-10-27 14:54:56 +0530

    commit

--- sample.py
+++ sample.py
@@ -1,2 +1,7 @@
 
-print('hello')
+import cgi;
+import cgitb;cgitb.enable()
+
+print("Content-Type: text/html")
+print ("") #use this double quote print statement to add a blank line in the script
+print("Hello python")
